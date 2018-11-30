08ca965f5861364d0cdca85610087cde22f3831f
zzccchen <2923400952@qq.com>
2018-12-01 00:45:24 +0800

    禁用 GET_ALL 添加 GET_LIMIT 和 GET_COUNT 方法

--- database/sql_set.py
+++ database/sql_set.py
@@ -13,9 +13,10 @@ class SqlSet():
 
     DB = "testsmallwei"
     # 使用 %s 是为了使用 pymysql 自带的 execute 防注入
-    GET_ALL = "SELECT {0} FROM {1}.{2} WHERE {3} = %s;"
     GET_ONE = "SELECT {0} FROM {1}.{2} WHERE {3} = %s LIMIT 1;"
-    GET_LIKE = "SELECT {0} FROM {1}.{2} WHERE {3} LIKE %s LIMIT %s, %s;"
+    # 禁用 GET_ALL，必须带 limit 限制
+    GET_LIMIT = "SELECT {0} FROM {1}.{2} WHERE {3} {4} %s LIMIT %s, %s;"
+    GET_COUNT = "SELECT COUNT(*) AS `count` FROM {0}.{1} WHERE {2} {3} %s;"
 
     @staticmethod
     async def get(sql_dict, fetch="one"):
@@ -28,16 +29,19 @@ class SqlSet():
                         sql_dict["key"])
                     await cursor.execute(sql, (sql_dict["value"]))
                     return cursor.fetchone()
-                elif fetch == "all":
-                    sql = SqlSet.GET_ALL.format(
+                elif fetch == "count":
+                    sql = SqlSet.GET_COUNT.format(
+                        SqlSet.DB, sql_dict["table"],
+                        sql_dict["key"], sql_dict["exp"])
+                    await cursor.execute(sql, (sql_dict["value"]))
+                    return cursor.fetchone()
+                elif fetch == "limit":
+                    sql = SqlSet.GET_LIMIT.format(
                         sql_dict["names"], SqlSet.DB, sql_dict["table"],
-                        sql_dict["key"])
-                elif fetch == "like":
-                    sql = SqlSet.GET_LIKE.format(
-                        sql_dict["names"], SqlSet.DB, sql_dict["table"],
-                        sql_dict["key"])
-                await cursor.execute(sql, (sql_dict["value"]))
-                return cursor.fetchall()
+                        sql_dict["key"], sql_dict["exp"])
+                    await cursor.execute(sql, (sql_dict["value"],
+                                               sql_dict["limit1"], sql_dict["limit2"]))
+                    return cursor.fetchall()
 
     @staticmethod
     def names_join(names_dict, names):
@@ -45,6 +49,10 @@ class SqlSet():
             return ", ".join([value for value in names_dict.values()])
         else:
             return ", ".join([names_dict.get(key) for key in names])
+
+    @staticmethod
+    def page2limit(page, pagesize):
+        return (page-1)*pagesize
 
     @staticmethod
     async def get_dictionary(value):
@@ -93,22 +101,26 @@ class SqlSet():
         }
         return await SqlSet.get(sql_dict)
 
-    # @staticmethod
-    # async def get_student_info_like(names, key, value, page, pagesize):
-    #     """模糊查询学生信息表.
+    @staticmethod
+    async def get_student_info_like(names, key, value, page, pagesize):
+        """模糊查询学生信息表.
 
-    #     Args:
-    #         names: 需要返回的列名(list)，当传入 ["*"] 时为全选
-    #         key: 查询基于的键名（基于模块变量名，与表无关）
-    #         value: 查询基于的键值
-    #     Returns:
-    #         由传入参数 names 决定，最多包括：
-    #         `name`, `stuid`, `cardno`, `qq`, `dept`, `major`, `grade`
-    #     """
-    #     sql_dict = {
-    #         "names": SqlSet.names_join(SqlSet.STUDENT_INFO_NAME_DICT, names),
-    #         "table": "student_info",
-    #         "key": "`{}`".format(key),
-    #         "value": value
-    #     }
-    #     return await SqlSet.get(sql_dict)
+        Args:
+            names: 需要返回的列名(list)，当传入 ["*"] 时为全选
+            key: 查询基于的键名（基于模块变量名，与表无关）
+            value: 查询基于的键值
+        Returns:
+            由传入参数 names 决定，最多包括：
+            `name`, `stuid`, `cardno`, `qq`, `dept`, `major`, `grade`
+            和 {"count": 符合查询条件总个数统计}
+        """
+        sql_dict = {
+            "names": SqlSet.names_join(SqlSet.STUDENT_INFO_NAME_DICT, names),
+            "table": "student_info",
+            "key": "`{}`".format(key),
+            "exp": "LIKE",
+            "value": value,
+            "limit1": SqlSet.page2limit(page, pagesize),
+            "limit2": pagesize
+        }
+        return await SqlSet.get(sql_dict, "limit"), await SqlSet.get(sql_dict, "count")
--- handler/translate/name_to_info_l_handler.py
+++ handler/translate/name_to_info_l_handler.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 # 此接口为名字转身份信息接口
 
+from asyncio import events
 import traceback
 
-import pymysql
-from tornado import gen
-import tornado.web
-
 from auth import jwtauth
-from database import db_pool
+from database import SqlSet
 from handler import BaseHandler
 from log import LogBase
 from route import app
@@ -18,36 +15,10 @@ logger = LogBase().get_logger("NameToInfoL")
 @app.route(r'/translate/nameToInfoL')
 @jwtauth
 class NameToInfoLHandler(BaseHandler):
-    """
-    名字转身份信息 handler
-    """
+    """名字转身份信息 handler."""
     INFO = {"author": "zzccchen", "version": "2.0"}
 
-    SELECT_SQL = """
-        SELECT
-            `name`,
-            `stuNo` AS `stuid`,
-            `cardNo` AS `cardno`,
-            `QQ` AS `qq`,
-            `dept`,
-            `major`,
-            `grade`
-        FROM
-            testsmallwei.student_info
-        WHERE
-            `name` LIKE %s
-        LIMIT %s , %s;"""
-
-    COUNT_SQL = """
-        SELECT 
-            COUNT(*) AS `count`
-        FROM
-            testsmallwei.student_info
-        WHERE
-            `name` LIKE %s;"""
-
-    @gen.coroutine
-    def get(self, *args, **kwargs):
+    async def get(self, *args, **kwargs):
         name = self.get_argument('name', None)
         page = self.get_argument_page()
         pagesize = self.get_argument_pagesize()
@@ -57,28 +28,18 @@ class NameToInfoLHandler(BaseHandler):
         if not name:
             return self.write_error_f(40010)
 
-        with (yield db_pool.Connection()) as conn:
-            try:
-                with conn.cursor(pymysql.cursors.DictCursor) as cursor:
-                    name = "%{}%".format(name)
-                    yield cursor.execute(self.SELECT_SQL, (name, (page-1)*pagesize, pagesize))
-                    info_list = cursor.fetchall()
-                    yield conn.commit()
-
-                    yield cursor.execute(self.COUNT_SQL, (name))
-                    count = cursor.fetchone()
-                    yield conn.commit()
-
-                    if not info_list and not count:
-                        return self.write_error_f(4044)
-
-                    self.pagination = {
-                        "page": page,
-                        "pagesize": pagesize,
-                        "total": count.get("count")
-                    }
-                    return self.write_json_f(info_list)
-
-            except Exception as e:
-                logger.error(traceback.format_exc())
-                return self.write_error_f(5001)
+        try:
+            name = "%{}%".format(name)
+            info_list, count = await SqlSet.get_student_info_like(
+                ["*"], "name", name, page, pagesize)
+            if not info_list and not count:
+                return self.write_error_f(4044)
+            self.pagination = {
+                "page": page,
+                "pagesize": pagesize,
+                "total": count.get("count")
+            }
+            return self.write_json_f(info_list)
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            return self.write_error_f(5001)

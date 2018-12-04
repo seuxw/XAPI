dfff0f25e943484ae0522853c082f372e93abb27
zzccchen <2923400952@qq.com>
2018-12-04 23:20:06 +0800

    简化代码模型

--- auth/auth.py
+++ auth/auth.py
@@ -5,12 +5,23 @@ import configparser
 
 import jwt
 
-INFO = {"author": "smallwei", "version": "2.1"}
-
 
 class JwtAuth(object):
     """JWT 身份验证类."""
+
+    INFO = {"author": "smallwei", "version": "2.1"}
+
     __instance = None
+
+    JWT_OPTIONS = {
+        'verify_signature': True,
+        'verify_exp': True,
+        'verify_nbf': False,
+        'verify_iat': True,
+        'verify_iss': True,
+        'verify_aud': True
+    }
+    TIMEOUT = 15552000  # 180 天
 
     def __new__(cls, *args, **kwargs):
         """单例模式实现."""
@@ -27,15 +38,6 @@ class JwtAuth(object):
         self.AUTHORIZATION_METHOD = conf["AUTH_CFG"]["AUTHORIZATION_METHOD"]
         self.SECRET_KEY = conf["AUTH_CFG"]["SECRET_KEY"]
         self.ALGORITHMS = conf["AUTH_CFG"]["ALGORITHMS"]
-        self.JWT_OPTIONS = {
-            'verify_signature': True,
-            'verify_exp': True,
-            'verify_nbf': False,
-            'verify_iat': True,
-            'verify_iss': True,
-            'verify_aud': True
-        }
-        self.TIMEOUT = 15552000  # 180 天
         # 用户类别 0-普通 10-VIP 20-管理 30-超级管理
         self.common = self._jwt(0)
         self.vip = self._jwt(10)
@@ -58,12 +60,12 @@ class JwtAuth(object):
                     get_auth = handler.request.headers.get(
                         self.AUTHORIZATION_HEADER)
                     if not get_auth:
-                        handler.INFO = INFO
+                        handler.INFO = self.INFO
                         return handler.write_error_f(4012)
 
                     auth_parts = get_auth.split()
                     if not self._is_valid_header(auth_parts):
-                        handler.INFO = INFO
+                        handler.INFO = self.INFO
                         return handler.write_error_f(4011)
 
                     token = auth_parts[1]
@@ -74,7 +76,7 @@ class JwtAuth(object):
                             options=self.JWT_OPTIONS,
                             algorithms=self.ALGORITHMS)
                     except Exception as err:
-                        handler.INFO = INFO
+                        handler.INFO = self.INFO
                         return handler.write_error_f(4011)
                     # TODO:
                     print(user)

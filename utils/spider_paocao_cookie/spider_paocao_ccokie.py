c92851c5722ed1002a58b3b9f4572cb132485b6e
zzccchen <2923400952@qq.com>
2018-12-06 18:32:20 +0800

    添加验证失效重试

--- utils/spider_paocao_cookie/spider_paocao_ccokie.py
+++ utils/spider_paocao_cookie/spider_paocao_ccokie.py
@@ -36,48 +36,63 @@ def sele():
         # driver.get("http://zccx.seu.edu.cn")
         driver.set_window_size(1200, 800)
 
-        driver.get_screenshot_as_file("utils/spider_paocao_cookie/screen.png")
-        element = driver.find_element_by_css_selector(
-            "body > table:nth-child(2) > tbody > tr:nth-child(2) > td > table:nth-child(1) > tbody > tr:nth-child(5) > td:nth-child(4) > img")
+        def get_code():
+            driver.get_screenshot_as_file(
+                "utils/spider_paocao_cookie/screen.png")
+            element = driver.find_element_by_css_selector(
+                "body > table:nth-child(2) > tbody > tr:nth-child(2) > td > table:nth-child(1) > tbody > tr:nth-child(5) > td:nth-child(4) > img")
 
-        left = int(element.location['x'])
-        top = int(element.location['y'])
-        right = int(element.location['x'] + element.size['width'])
-        bottom = int(element.location['y'] + element.size['height'])
-        # TODO： Fix
-        im = Image.open('utils/spider_paocao_cookie/screen.png')
-        im = im.crop((left, top, right, bottom))
-        w, h = im.size
-        im = im.resize((w*3, h*3))
-        im.save('utils/spider_paocao_cookie/screen1.png')
+            left = int(element.location['x'])
+            top = int(element.location['y'])
+            right = int(element.location['x'] + element.size['width'])
+            bottom = int(element.location['y'] + element.size['height'])
+            # TODO： Fix
+            im = Image.open('utils/spider_paocao_cookie/screen.png')
+            im = im.crop((left, top, right, bottom))
+            w, h = im.size
+            im = im.resize((w*3, h*3))
+            code = pytesseract.image_to_string(im)
+            if code:
+                print(code)
+                return code
+            else:
+                driver.refresh()
+                return get_code()
 
-        code = pytesseract.image_to_string(im)
-        print(code)
+        def login(code):
+            elem1 = driver.find_element_by_id("IDToken1")
+            elem2 = driver.find_element_by_id("IDToken2")
+            elem3 = driver.find_element_by_name("inputCode")
 
-        im.close()
+            time.sleep(3)
 
-        elem1 = driver.find_element_by_id("IDToken1")
-        elem2 = driver.find_element_by_id("IDToken2")
-        elem3 = driver.find_element_by_name("inputCode")
+            conf = configparser.ConfigParser()
+            conf.read("utils/spider_paocao_cookie/paocao.cfg")
+            elem1.send_keys(conf["CARD_INFO"]["CARDNO"])
+            elem2.send_keys(conf["CARD_INFO"]["CARDPSWD"])
+            elem3.send_keys(code)
 
-        time.sleep(3)
+            login = driver.find_element_by_css_selector(
+                "body > table:nth-child(2) > tbody > tr:nth-child(2) > td > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(5) > img")
+            login.click()
 
-        conf = configparser.ConfigParser()
-        conf.read("utils/spider_paocao_cookie/paocao.cfg")
-        elem1.send_keys(conf["CARD_INFO"]["CARDNO"])
-        elem2.send_keys(conf["CARD_INFO"]["CARDPSWD"])
-        elem3.send_keys(code)
+        def get_jsid():
+            try:
+                code = get_code()
+                login(code)
+                jsid = ""
+                for i in driver.get_cookies():
+                    if i['name'] == 'JSESSIONID':
+                        jsid = i['value']
+                        break
+                time.sleep(3)
+                return jsid
+            except Exception:
+                time.sleep(7)
+                driver.refresh()
+                get_jsid()
 
-        login = driver.find_element_by_css_selector(
-            "body > table:nth-child(2) > tbody > tr:nth-child(2) > td > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(5) > img")
-        login.click()
-
-        jsid = ""
-        for i in driver.get_cookies():
-            if i['name'] == 'JSESSIONID':
-                jsid = i['value']
-                break
-        time.sleep(3)
+        jsid = get_jsid()
         driver.quit()
         logger.info("Success sele")
         return jsid
